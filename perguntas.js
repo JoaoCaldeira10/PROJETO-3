658338571199d8b1c1376f8b335f407c4c32f70e
JoaoCaldeira10 <160407845+JoaoCaldeira10@users.noreply.github.com>
2024-11-29 23:11:28 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'JOGOS',
+    'Quais irmãos encanadores estrelam a franquia Super Mario da Nintendo?',
+    'Mário e Luigi'
+)
+
+criaCartao(
+    'JOGOS',
+    'Qual jogo de terror desafia os jogadores com animais animatrônicos sobreviventes?',
+    'Cinco noites no Freddy'
+)
+
+criaCartao(
+    'JOGOS',
+    'Qual herói da Sony é extremamente protetor com a figura de sua filha adotiva?',
+    'Joel(The Last of Us)'
+)
+
+criaCartao(
+    'MARVEL',
+    'Em que ano foi lançado o primeiro filme do Homem de Ferro, dando início ao Universo Cinematográfico da Marvel?',
+    '2008'
+)
+
+criaCartao(
+    'MARVEL',
+    'Qual é o filme da Marvel de menor bilheteria em 2023?',
+    'As Marvels'
+)
+
+criaCartao(
+    'MARVEL',
+    ' Em que cena de pós-crédito do filme Thanos apareceu pela primeira vez?',
+    'Os Vingadores'
+)
